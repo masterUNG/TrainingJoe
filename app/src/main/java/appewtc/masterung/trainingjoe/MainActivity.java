a61ff3cef2836d1142240f0dde2d2c48cfd21dfd
masterUNG <phrombutr@gmail.com>
2017-04-14 11:12:45 +0700

    Intent to Regis OK

--- app/src/main/java/appewtc/masterung/trainingjoe/MainActivity.java
+++ app/src/main/java/appewtc/masterung/trainingjoe/MainActivity.java
@@ -1,14 +1,54 @@
 package appewtc.masterung.trainingjoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initial View
+        initialView();
+
+        //Controller
+        controller();
+
+    }   // Main Method
+
+    private void controller() {
+        textView.setOnClickListener(MainActivity.this);
+        button.setOnClickListener(MainActivity.this);
     }
 
-}
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //For TextView
+        if (view == textView) {
+            Intent intent = new Intent(MainActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        }
+
+    }   // onClick
+
+}   // Main Class
